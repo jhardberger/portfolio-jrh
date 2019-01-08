a52332e5a065cdca5bb957ba900aa5d0a36158cc
John <john.hardberger@gmail.com>
2019-01-08 17:38:40 -0600

    played with some parallax stuff on the name page, reworking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import Nav from './Nav/index';
-import Name from './Name/index';
 import WorkContainer from './WorkContainer/index';
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <ParallaxProvider>
-        <Name />
-        <div class="body">
-          <h2>writer | programmer | your new best friend</h2>
-          <p class="intro">wadduup waddup waddup</p>
-          <WorkContainer />
-        </div>
-      </ParallaxProvider>
+      <div class="body">
+        <Nav />
+        <h1>John Hardberger</h1>
+        <h2>writer | programmer | your new best friend</h2>
+        <br/>
+        <p class="intro">wadduup waddup waddup</p>
+        <br />
+        <WorkContainer />
+      </div>
     );
   }
 }
