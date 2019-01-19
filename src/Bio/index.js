68abaeb441f31393d2a054924844abfafd772950
John <john.hardberger@gmail.com>
2019-01-19 13:57:13 -0600

    continuing to style bio/profile

--- src/Bio/index.js
+++ src/Bio/index.js
@@ -6,19 +6,18 @@ const Bio = () => {
 	return(
 		<div className='bio'>
 			<Fade clear>
-				<p className='drop-cap-graf'>Howdy! My name's John Hardberger, and I'm writer, web developer, and all-around audio tinkerer. I'm originally from Texas, but for the last seven years I've hung my hat in Chicagoland, USA.
-				</p>
+				<div className='row-1'>
+					<p className='drop-cap-graf'>Howdy! My name's John Hardberger, and I'm writer, web developer, and all-around audio tinkerer. I'm originally from Texas, but for the last seven years I've hung my hat in Chicagoland, USA.
+					</p>
+				</div>
 			</Fade>
-			<Fade clear>
-				<p>As a writer, I tend to focus on Chicago's thriving music, comedy, and art scenes, but I've also been known to cover immigration, education and real estate, and to write the occasional review.
-				The thread that I feel unites my work is a genuine interest the people who make Chicago tick. 
-				I firmly believe that all of us have a story to tell, and that what might on the surface seem mundane can contain the seeds of the transcendent. SEE MY WRITING PORTFOLIO</p>
-			</Fade>
-			<Fade clear>	
-				<p>My work as a developer is still, well, developing: I graduated from General Assembly's intensive Web Development Immersive in December, and am currently seeking opportunities to expand my portfolio and continue my education.
-				While I'm quite confident on the back end, its front end, design-focused work that most excites my creative interests.
-				To that end, I strive to craft beautiful, simple projects that stem from thoughtful consideration of the user. SEE MY DEV PORTFOLIO</p>
-			</Fade>
+			<div className='row-2'>
+				<div className='prof-pic-container'>
+					<img src='/prof-pic.jpeg' className='prof-pic' />
+				</div>
+				<p className='paragraph-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus. Curabitur dui felis, fringilla quis libero non, ullamcorper semper mi. Aenean tempus, sem vitae ullamcorper convallis, ante ex cursus dolor, non feugiat nisi neque vitae ex. Quisque in blandit velit. Vestibulum justo leo, facilisis in aliquet at, blandit sit amet ligula. Aenean ut libero finibus, imperdiet lectus nec, feugiat magna. Suspendisse congue ligula in ultrices auctor. Suspendisse magna est, interdum id diam id, congue porta nulla. Suspendisse elementum metus ut libero egestas, sit amet fermentum nibh varius. Nulla turpis nunc, maximus eu metus ac, euismod varius nibh. Sed sit amet massa tincidunt, iaculis leo at, elementum sapien. Morbi quis sagittis leo.</p>
+			</div>
+			
 			<div className='spacer' >
 			</div>
 		</div>
