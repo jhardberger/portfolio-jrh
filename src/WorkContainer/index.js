fb735458b3066522cf7df89716907f95856eb116
John <john.hardberger@gmail.com>
2019-01-22 18:00:21 -0600

    implemented semi-successful parallax in workcontainer projects

--- src/WorkContainer/index.js
+++ src/WorkContainer/index.js
@@ -1,41 +1,76 @@
 import React, { Component } from 'react';
 
+import { Parallax } from 'react-scroll-parallax';
+import { Fade } 	from 'react-reveal';
+
 
 class WorkContainer extends Component {
 	render(){
 		return(
 			<div className="work-container">
+			
 				<div className="project" id="project-1">
-					<img src='/prof-pic.jpeg' className='project-img' />
+				
 					<div className='project-body'>
-						<h1>Project Title</h1>
-						<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
-						</p>
+						<Parallax offsetYMax='-30' offsetYMin='30' slowerScrollRate >
+							<Fade botton opposite>
+								<img src='/prof-pic.jpeg' className='project-img' />
+							</Fade>
+						</Parallax>
+						<Parallax offsetYMax='30' offsetYMin='-30' slowerScrollRate >
+							<Fade top opposite>
+								<h1>Project Title</h1>
+								<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
+								</p>
+							</Fade>
+						</Parallax>
 					</div>
 				</div>
+
+				
 				<div className="project" id="project-2">
-					<img src='/prof-pic.jpeg' className='project-img' />
-					<div className='project-body'>
-						<h1>Project Title</h1>
-						<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
-						</p>
-					</div>
+					<Parallax offsetYMax='-30' offsetYMin='30' slowerScrollRate >
+						<Fade botton opposite>
+							<img src='/prof-pic.jpeg' className='project-img' />
+						</Fade>
+					</Parallax>
+					<Parallax offsetYMax='30' offsetYMin='-30' slowerScrollRate >
+						<Fade top opposite className='project-body'>
+							<h1>Project Title</h1>
+							<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
+							</p>
+						</Fade>
+					</Parallax>
 				</div>
 				<div className="project" id="project-3">
-					<img src='/prof-pic.jpeg' className='project-img' />
-					<div className='project-body'>
-						<h1>Project Title</h1>
-						<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
-						</p>
-					</div>
+					<Parallax offsetYMax='-30' offsetYMin='30' slowerScrollRate >
+						<Fade botton opposite>
+							<img src='/prof-pic.jpeg' className='project-img' />
+						</Fade>
+					</Parallax>
+					<Parallax offsetYMax='30' offsetYMin='-30' slowerScrollRate >
+						<Fade top opposite className='project-body'>
+							<h1>Project Title</h1>
+							<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
+							</p>
+						</Fade>
+					</Parallax>
 				</div>
 				<div className="project" id="project-4">
-					<img src='/prof-pic.jpeg' className='project-img' />
-					<div className='project-body'>
-						<h1>Project Title</h1>
-						<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
-						</p>
-					</div>
+					<Parallax offsetYMax='-30' offsetYMin='30' slowerScrollRate >
+						<Fade botton opposite>
+								<img src='/prof-pic.jpeg' className='project-img' />
+							</Fade>
+					</Parallax>
+					<Parallax offsetYMax='30' offsetYMin='-30' slowerScrollRate >
+						<Fade top opposite className='project-body'>
+							<h1>Project Title</h1>
+							<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum volutpat risus, ac fringilla ligula vulputate eu. Vivamus nec velit non nibh lobortis dapibus nec nec velit. Sed aliquam aliquam maximus.
+							</p>
+						</Fade>
+					</Parallax>
+				</div>
+				<div className='spacer'>
 				</div>
 			</div>
 		)
