b64cf9ac732ae720e615b9bf6b6d7519522891cb
John <john.hardberger@gmail.com>
2019-02-05 15:00:20 -0600

    added resume

--- src/ResumeModal/index.js
+++ src/ResumeModal/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import { Document } 	from 'react-pdf';
 import { Icon, Modal } 	from 'semantic-ui-react';
 
 const ResumeModal = (props) => {
 	return(
 		<div className='resume'>
-			<Modal open={props.showResume} dimmer='blurring'>
-				<Modal.Header><h1>résumé</h1></Modal.Header>
+			<Modal open={props.showResume} dimmer='blurring' >
+				<Modal.Header><h1 className='resume-header'>résumé</h1></Modal.Header>
 				<Modal.Content>
-					<Document file='../public/johnhardberger_resume_2019.pdf' />
-					<a href='javascript:;'><Icon name='remove' onClick={props.handleResume}/></a>	
+					<div className='resume-content'>
+						<img src='johnhardberger_resume_2019.png' className='resume-img' />
+						<a href='javascript:;'><Icon name='remove' onClick={props.handleResume}/></a>	
+					</div>
 				</Modal.Content>
 			</Modal>
 		</div>
