bec956e1614fc5f4d44e987004a71e399f2b9270
John <john.hardberger@gmail.com>
2019-03-20 19:29:50 -0500

    added design thumbs to design component

--- src/Design/index.js
+++ src/Design/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 import { Fade } 		from 'react-reveal';
+import { Image } 		from 'semantic-ui-react';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
 const Design = (props) => {
+	const designURLS = ['design1.jpg', 'design2.jpg', 'design3.jpg', 'design4.jpg'];
+	const designs = designURLS.map((design, i) => {
+		return(
+			<Image 
+				src={design} 
+				className='design-thumb' 
+				id={'design-' + i} 
+				size='small' 
+			/>
+		)
+	});
+
 	return(
 		<Fade left opposite when={props.showDesign} >
 			<div className='design'>
 				<ScrollableAnchor id={'design'}>
 					<div className='design-header'>
-						wassssssup
+						<p>I 've always had a deep love of simple, beautiful graphic design, and while that love has informed my approach to a variety of creative roles, growing and learning as a designer has also at times been an endeavor all its own. Below are some flat components I've designed through the years.</p> 
 					</div>
 				</ScrollableAnchor>
 				<div className='design-port'>
+					{designs}
+				</div>
+				<div className='design-footer'>
+					<p>I'm also dabbling in web-based design these days—you can see some examples of that through my dev work (above), the Photo Carousel (also above), and this website (which looks pretty good, if I do say so myself).</p>
 				</div>
 			</div>
 		</Fade>
