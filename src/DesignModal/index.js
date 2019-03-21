38c167af6ca0235e8ebbecc8b745e49b73f044b4
John <john.hardberger@gmail.com>
2019-03-20 20:07:12 -0500

    classed up the modal joint with some css, simplified structure

--- src/DesignModal/index.js
+++ src/DesignModal/index.js
@@ -5,20 +5,18 @@ import { Image, Modal, Icon } from 'semantic-ui-react';
 
 const DesignModal = (props) => {
 	return(
-		<div className='design-modal'>
-			<Modal basic open={props.designModal} >
-				<div className='modal-inner'>
-					<a href='javascript:;'>
-						<Icon name='remove' onClick={props.handleDesignModal}/>
-					</a>
-					<Image
-						src={props.designModal}
-						size='massive'
-						className='design-big'
-					/>
-				</div>
-			</Modal>
-		</div>
+		<Modal basic open={props.designModal} >
+			<div className='design-modal'>
+				<a href='javascript:;'>
+					<Icon name='remove' onClick={props.handleDesignModal}/>
+				</a>
+				<Image
+					src={props.designModal}
+					size='massive'
+					className='design-big'
+				/>
+			</div>
+		</Modal>
 	)
 }
 export default DesignModal;
