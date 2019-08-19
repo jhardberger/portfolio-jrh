ca0441194a9c53a319e0d34f0e08228bf3460cb6
John <john.hardberger@gmail.com>
2019-08-19 12:11:16 -0500

    updated copy on selecteddev

--- src/SelectedDev/index.js
+++ src/SelectedDev/index.js
@@ -68,90 +68,82 @@ class SelectedDev extends Component {
 
 	render(){	
 		return(
-			<div className='selected-dev'>
-				<h1>web dev</h1>
-				<ScrollableAnchor id={'dev'}>
-					<div className='dev-header'>
-						i have always been been fascinated by graphic and web design, and in December 2018, I decided to parlay that passion into a new set of skills through General Assembly's Web Development Immersive. In ten weeks I went from knowing a few HTML tags to building full stack web apps from scratch. Here are some apps I wrote while at GA and in my free time after completing the course.
-					</div>
-				</ScrollableAnchor>
+			<Fade right >
 
-				<div className='projects'>
+				<div className='selected-dev'>
+					<h1>web dev</h1>
+					<ScrollableAnchor id={'dev'}>
+						<div className='dev-header'>
+							i have always been been fascinated by graphic and web design, and in December 2018, I decided to parlay that passion into a new set of skills through General Assembly's Web Development Immersive. Here are some apps I wrote while at GA and in my free time after completing the course.
+						</div>
+					</ScrollableAnchor>
 
-					<div className='project'>
-						<div className='project-body' id='project-odd'>
-							<div className='project-head' id='project-head-odd' onMouseEnter={this.showDemo1} onMouseLeave={this.hideDemo1}>
-								<h1>
-									<a href='https://github.com/jhardberger/sm_client'>beatgrinder</a>
-								</h1>
+					<Fade right cascade >
 
-								<Icon name='chevron left' className='demo-arrow' />
-								<Fade right when={this.state.show1} >
-									<div className='demo'><h2>click to demo</h2></div>
-								</Fade>
+						<div className='projects'>
+
+							<div className='project'>
+								<div className='project-body' id='project-odd'>
+									<div className='project-head' id='project-head-odd' onMouseEnter={this.showDemo1} onMouseLeave={this.hideDemo1}>
+										<h1>
+											<a href='https://github.com/jhardberger/sm_client'>beatgrinder</a>
+										</h1>
+
+										<Icon name='chevron left' className='demo-arrow' />
+										<Fade right when={this.state.show1} >
+											<div className='demo'><h2>see on github</h2></div>
+										</Fade>
+									</div>
+									<div className='project-copy'>
+										My enterprise project for GA: a full-stack app that uses Spotify's API to churns out free-range, organic music recommendations.  
+									</div>
+								</div>
 							</div>
-							<div className='project-copy'>
-								some crap about beatgrinder
+
+							<div className='project'>
+								<div className='project-body' id='project-even'>
+									<div className='project-head' id='project-head-even' onMouseEnter={this.showDemo2} onMouseLeave={this.hideDemo2}>
+										<h1>
+											<a href='https://github.com/jhardberger/Project_3_backend'>mapulate</a>
+										</h1>
+										
+										<Icon name='chevron left' className='demo-arrow' />
+										<Fade right when={this.state.show2} >
+											<div className='demo'><h2>see on github</h2></div>
+										</Fade>
+									</div>
+									<div className='project-copy'>
+										Built in collaboration with a classmate, Mapulate is a population guessing game and learning tool for the census nerd in your life.
+									</div>
+								</div>
+							</div>
+
+							<div className='project'>
+								<div className='project-body' id='project-odd'>
+									<div className='project-head' id='project-head-odd' onMouseEnter={this.showDemo3} onMouseLeave={this.hideDemo3}>
+										<h1>
+											<a href='https://github.com/jhardberger/Project_2'>waxxy</a>
+										</h1>
+										
+										<Icon name='chevron left' className='demo-arrow' />
+										<Fade right when={this.state.show3} >
+											<div className='demo'><h2>see on github</h2></div>
+										</Fade>
+									</div>
+									<div className='project-copy'>
+										Another collaborative effort, Waxxy is a social record-collecting app for veteran cratediggers and freshly-pressed wax addicts alike. 
+									</div>
+								</div>
 							</div>
 						</div>
+					</Fade>
+
+					<div className='dev-footer'>
+					Over the ten-week web development course, I went from knowing a few HTML tags to building full stack web apps from scratch. That said, these projects are all very much still works-in-progress. I look forward to sharing my future, nicer apps with you as they (ahem) develop.
 					</div>
 
-					<div className='project'>
-						<div className='project-body' id='project-even'>
-							<div className='project-head' id='project-head-even' onMouseEnter={this.showDemo2} onMouseLeave={this.hideDemo2}>
-								<h1>
-									<a href='https://github.com/jhardberger/Project_3_backend'>mapulate</a>
-								</h1>
-								
-								<Icon name='chevron left' className='demo-arrow' />
-								<Fade right when={this.state.show2} >
-									<div className='demo'><h2>click to demo</h2></div>
-								</Fade>
-							</div>
-							<div className='project-copy'>
-								some crap about mapulate
-							</div>
-						</div>
-					</div>
-
-					<div className='project'>
-						<div className='project-body' id='project-odd'>
-							<div className='project-head' id='project-head-odd' onMouseEnter={this.showDemo3} onMouseLeave={this.hideDemo3}>
-								<h1>
-									<a href='https://github.com/jhardberger/Project_2'>waxxy</a>
-								</h1>
-								
-								<Icon name='chevron left' className='demo-arrow' />
-								<Fade right when={this.state.show3} >
-									<div className='demo'><h2>click to demo</h2></div>
-								</Fade>
-							</div>
-							<div className='project-copy'>
-								some crap about waxxy
-							</div>
-						</div>
-					</div>
-
-					<div className='project' >
-						<div className='project-body' id='project-even'>
-							<div className='project-head' id='project-head-even' onMouseEnter={this.showDemo4} onMouseLeave={this.hideDemo4}>
-								<h1>
-									<a href='https://github.com/jhardberger/Project-1-game'>qwerty_derby</a>
-								</h1>
-								
-								<Icon name='chevron left' className='demo-arrow' />
-								<Fade right when={this.state.show4} >
-									<div className='demo'><h2>click to demo</h2></div>
-								</Fade>
-							</div>
-							<div className='project-copy'>
-								some crap about qd
-							</div>
-						</div>
-					</div>
 				</div>
-				
-			</div>
+			</Fade>
 		)
 	}
 }
--- src/SelectedWritings/index.js
+++ src/SelectedWritings/index.js
@@ -6,7 +6,7 @@ import ScrollableAnchor from 'react-scrollable-anchor';
 
 const SelectedWritings = (props) => {
 	return(
-		<Fade left opposite when={props.showWriting} >
+		<Fade left opposite >
 			<div className='selected-writings'>
 				<h1>writing</h1>
 				<ScrollableAnchor id={'writing'} >
@@ -14,7 +14,7 @@ const SelectedWritings = (props) => {
 						<p>for three years, I was a writer and reporter—mostly about fun stuff, mostly in Chicago—for <i>Chicago</i> magazine, the <i>Chicago Tribune</i> and ApartmentTherapy.com. Here are, in no particular order, a few personal-favorite stories I worked on in that time.</p>
 					</div>
 				</ScrollableAnchor>
-				<Fade left cascade opposite when={props.showWriting} >
+				<Fade left cascade  >
 					<ul className='writing-links'>
 						<li>
 							<a href='https://www.chicagotribune.com/dining/restaurants/ct-hot-dog-day-unexpected-places-food-0719-20170714-story.html'
