cbb30eb518d07845ba0a1b974b194878b6f46022
John <john.hardberger@gmail.com>
2019-08-19 13:19:57 -0500

    updated Css - enlarged pic

--- src/Bio/index.js
+++ src/Bio/index.js
@@ -15,7 +15,7 @@ const Bio = (props) => {
 			<Fade clear cascade>
 				<ScrollableAnchor id={'hi'}	>				
 					<div className='intro'>	
-						<p className='drop-cap-graf'>howdy! My name's John Hardberger. I'm front end web developer with a love of visual storytelling and clean, user-friendly design. I used to be a journalist, and many of the skills I picked up in that trade—communication, teamwork, and attention to detail—continue to inform my work. My primary mode of transportation is bicycle. I'm probably listening to music right now. I appreciate a good hot dog.</p>
+						<p className='drop-cap-graf'>howdy! My name is John Hardberger. I'm a writer, designer, and web developer.  My primary mode of transportation is bicycle. I probably have headphones in right now. I appreciate a good hot dog.</p>
 					</div>
 				</ScrollableAnchor>
 				<Waypoint onEnter={props.handleHideName} />	
@@ -25,15 +25,11 @@ const Bio = (props) => {
 					</div>
 					<div className='bio-copy'>
 						<h1>about me</h1>
-						<p> As a generally curious person with a wide range of interests, I struggled for a long time to find focus in my work life. I flirted with careers in graphic design, DJing, and podcasting before settling into a three-year editorial journey.
+						<p> As a generally curious person with a wide range of interests, I struggled for a long time to find focus in my work life. I flirted with careers in graphic design, DJing, and podcasting before setting out on an editorial journey that would last more than four years. In that time, I honed my skills as a writer, reporter, and editor. I learned how to engage with readers and sources, both through my writing and on social media. 
 						</p>
-						<p>These days, I'm making my home on the information highway—as a web developer with full stack chops and front end finesse, specializing in JavaScript and React. I've always been been fascinated by graphic and web design, and in December 2018, I decided to parlay that passion into a new career through General Assembly's intensive Web Development Immersive.  
-						</p>
-						<p>In ten weeks I went from knowing a few HTML tags to building full stack web apps from scratch. The course helped me approach programming on terms I could understand, while constantly challenging and encouraging me to put that understanding into practice. I'm extremely excited to test that growth, as well to continue my education as a coder, critical thinker, and creative problem solver.
-						</p>
-						<p>I’m currently seeking any work that will allow me to do just that. Below are some of the projects I developed while at General Assembly, some things I’ve worked in the time since graduation, and a few fun, non-dev passion projects. That includes this very website! It, like everything else here (including the guy in that picture over there), is a work in progress.
+						<p>But my passion for storytelling - I began to realize - extended beyond the frameworks of traditional journalism. On top of that, my interest in multi-media tools lingered even as I dove deeper and deeper into the editorial world. In 2018, I enrolled in a programming bootcamp to translate these ideas into a new set of skills, and emerged 10 weeks later a newly-minted web developer. 
 						</p> 
-						<p>If you like what you see and are interested in getting in touch, <span onClick={props.handleModal}><a href='javascript:;'>please drop me a line here</a></span>. 
+						<p>I believe that as a result of these wide-ranging skills and paths, I have become well-equipped to navigate the future of media, and to continue my lifelong education in storytelling. I'm currently seeking employment that will allow me to do just that. Below is some work that I feel represents what I'm capable off, both as a developer and as a writer. If you like what you see and are interested in getting in touch, <span onClick={props.handleModal}><a href='javascript:;'>please drop me a line here</a></span>. 
 						</p>
 					</div>
 				</div>
